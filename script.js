45ea94b3a8d707160ad398b242f9557b8d4ae640
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2022-11-10 17:45:48 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.body.addEventListener('click', (e) => {
+    const element = document.createElement('span');
+    element.className = 'circle';
+    element.style.left = e.clientX - 10 + 'px';
+    element.style.top = e.clientY - 10 + 'px';
+    document.body.append(element);
+    setTimeout(()=>{
+        element.remove();
+    },900);
+});
